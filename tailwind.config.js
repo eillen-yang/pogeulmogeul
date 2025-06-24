3911a045c2f45509d4b16e24494b0ba41d60dcc9
양아름 <yangareum0130@gmail.com>
2025-06-24 18:54:02 +0900

    chore : tailwind 설정 셋팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // next 13 이상 app directory 지원
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1.2rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1440px',
+      },
+    },
     extend: {
+      colors: {
+        primary: '#250996',
+        secondary: '#6366F1',
+        accent: '#f55454',
+        background: '#efeff4',
+        border: '#d7d7dc',
+        text: '#111827',
+      },
       fontFamily: {
         sans: ['Pretendard', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcss()],
 }
