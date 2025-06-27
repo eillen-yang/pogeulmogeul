be898413e21a7c07b7c7bc45fe8242a4a3e16bea
양아름 <yangareum0130@gmail.com>
2025-06-27 17:33:19 +0900

    publishing : step03완료

--- app/(auth)/auth/signup/step03/page.tsx
+++ app/(auth)/auth/signup/step03/page.tsx
@@ -3,11 +3,27 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div className="flex justify-center flex-col items-center">
-      <p>지금바로 포글모글의 다양한 서비스를 이용해보세요.</p>
-      <h2>회원가입이 완료되었습니다.</h2>
-      <div>
-        <Link href={'/'}>메인으로 돌아가기</Link>
-        <Link href={'/:username'}>마이페이지 둘러보기</Link>
+      <p className="mb-3 text-lg text-[var(--color-5)]">
+        지금바로 포글모글의 다양한 서비스를 이용해보세요.
+      </p>
+      <h2 className="pb-14 text-4xl font-bold text-center">
+        <span className="text-[var(--main-color)]">회원가입</span>이
+        <br />
+        완료되었습니다.
+      </h2>
+      <div className="flex flex-col items-center justify-center text-center gap-5 text-xl min-w-[420px]">
+        <Link
+          href={'/'}
+          className="p-4 w-full bg-[var(--main-color)] text-white rounded-4xl"
+        >
+          메인으로 돌아가기
+        </Link>
+        <Link
+          href={'/:username'}
+          className="p-4 w-full border border-[var(--color-5)] rounded-4xl"
+        >
+          마이페이지 둘러보기
+        </Link>
       </div>
     </div>
   )
