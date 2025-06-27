fb2b3cbb61d6d4268742e257ef138b4934367724
양아름 <yangareum0130@gmail.com>
2025-06-27 11:08:13 +0900

    publishing : header css 수정

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,11 +1,16 @@
+import Header from '@/app/_components/Header'
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <div className="flex items-center justify-center h-dvh">
-      {children}
-    </div>
+    <>
+      <Header />
+      <div className="flex items-center justify-center h-dvh">
+        {children}
+      </div>
+    </>
   )
 }
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
               </Link>
               <Link
                 href={'/auth/signup'}
-                className="block py-2 px-7 text-[var(--main-color)] bg-white hover:bg-[var(--main-color)] hover:text-white rounded-3xl border border-[var(--main-color)]"
+                className="block py-2 px-7 text-[var(--main-color)] bg-white hover:bg-[var(--main-color)] hover:text-white rounded-3xl border border-white hover:border hover:border-white"
               >
                 회원가입
               </Link>
