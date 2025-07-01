74abd5b10bd551161bf6c4e0f1ae49675fef1e98
양아름 <yangareum0130@gmail.com>
2025-07-01 23:43:02 +0900

    publishing : 내 프로필 등록이미지 퍼블리싱

--- app/(route)/_components/MyImages.tsx
+++ app/(route)/_components/MyImages.tsx
@@ -1,3 +1,55 @@
+import { faker } from '@faker-js/faker'
+import Image from 'next/image'
+import image_upload from '@/public/icon/img_upload.svg'
+
 export default function MyImages() {
-  return <div>등록 이미지 페이지</div>
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {/* {images.map((src, idx) => ( */}
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+      <img
+        src={faker.image.avatar()}
+        alt={`user image`}
+        className="w-full h-auto object-cover rounded-xl"
+      />
+
+      <div className="flex items-center justify-center w-full py-10 border border-[var(--color-2)] rounded-xl cursor-pointer">
+        <Image
+          className="object-cover"
+          src={image_upload}
+          alt="등록이미지 업로드"
+        />
+      </div>
+      {/* ))} */}
+    </div>
+  )
 }
