b559c741a69414b090a798aa8b4868555794641a
양아름 <yangareum0130@gmail.com>
2025-07-16 11:57:05 +0900

    fix: add server-side redirect from / to /auth/login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react'
 import Layout from './(route)/layout'
+import { redirect } from 'next/navigation'
 
 type Props = {
   children: React.ReactNode
 }
 
-export default function Home({ children }: Props) {
-  return <Layout children={children} />
+export default function Home() {
+  redirect('/auth/login')
 }
