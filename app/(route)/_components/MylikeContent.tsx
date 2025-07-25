654d10de1b3c0ed89e3823c2dee651d28a56d53b
양아름 <yangareum0130@gmail.com>
2025-07-25 13:21:23 +0900

    publishing : 즐겨찾기 한 사람이 없을경우 예외처리

--- app/(route)/_components/MylikeContent.tsx
+++ app/(route)/_components/MylikeContent.tsx
@@ -7,6 +7,14 @@ interface MylikeContentProps {
 export default function MylikeContent({
   favorites,
 }: MylikeContentProps) {
+  if (favorites.length === 0) {
+    return (
+      <div className="flex items-center justify-center py-40 text-2xl font-semibold border border-[var(--main-color)] text-[var(--main-color)] rounded-3xl">
+        <p>즐겨찾기한 사용자가 없습니다.</p>
+      </div>
+    )
+  }
+
   return (
     <div className="grid grid-cols-2 gap-3">
       {favorites.map((fa: FavoriteUsers) => (
